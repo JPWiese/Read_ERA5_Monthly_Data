e5332743a493729d2fcdd531f77ec73781e43b72
JPWiese <57562387+JPWiese@users.noreply.github.com>
2020-02-21 10:15:52 -0500

    added files via upload

---
+++ era5_tabulate.r
@@ -0,0 +1,72 @@
+
+# This program converts the ERA5 monthly temperature data file
+# from NetCDF to CSV. The ERA5 data is on a 0.25 degree grid.
+# Because the grid spacing is so small, the resulting CSV file
+# is very big. To make the data more manageable, I drop all
+# grid points except for those that correspond with integer
+# degree values. This reduces the data down to 1/16 of its
+# original size. Using this approach, the resulting CSV
+# file is about 500 megabytes. To further reduce its size,
+# I've included parameters to restrict the range of years you
+# which to examine. Also, if you wish, you can isolate a
+# rectangular lat/lon area, dropping all grid points outside
+# of that area.
+
+# the data inputs needed to run this program can be download from here:
+# https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-land-monthly-means?tab=form
+
+
+Location_R_Code <- "D:/JPW/data_ERA5_Monthly"  # location of your R program
+
+ERA5_Data <- "D:/JPW/data_ERA5_Monthly/data_era5_monthly_temperature.nc"
+
+CSV_Output_File <<- "out_era5.csv"  # this output file is produced by this program
+
+# Note that although I requested data back to 1981, I think the NetCDF file
+# actually contains data back to 1979. This needs to be investigated further.
+# Note also that the web page indicates that the NetCDF format for the ERA5 data
+# is "experimental", suggesting, perhaps, that download errors are possible.
+# Alternatively, one can use the GRIB file format, but I couldn't figure out
+# how to deal with GRIB files using R.
+
+Year_First <<- 1979 # the first year you wish to extract (no earlier than 1979)
+Year_Last  <<- 2019 # the last year you wish to extract (no later than 2019)
+
+Month_First <<- 1   # 1 = jan, 2 = feb, etc
+Month_Last  <<- 12 
+
+Extract_Rectangular_Area <<- TRUE  # false = entire planet, true = a particular area
+
+UseOneDegreeSpacing <<- TRUE  #    set to TRUE if you want to drop all non-integer latitude
+                              #    and longitude grid points. This reduces the data to 
+                              #    1/16th its original size.
+
+# use the min and max boundaries below to specify a rectangular area
+
+Area_Lat_Min <<- 25  # degrees north
+Area_Lat_Max <<- 50  # degrees north
+
+Area_Lon_Min <<- 180  # degrees east
+Area_Lon_Max <<- 300  # degrees east
+
+cat("\014")
+
+# you need the ncdf4 library, but you only need to install it once
+
+#install.packages("ncdf4")  
+#install.packages("matrixStats")
+
+library("ncdf4")
+library("matrixStats")
+
+setwd(Location_R_Code)
+
+source("era5_tabulate_support.r")
+
+cat("\014")
+
+Create_CSV_File(ERA5_Data)
+
+print("Success! The CSV output file was generated. ")
+
+print("Note that the data in the output file are expressed in degrees Celsius.")
---
+++ era5_tabulate_support.r
@@ -0,0 +1,154 @@
+Create_CSV_File <- function(sFile)
+{
+  
+  # 1440 longitude steps, where each step = 0.25 degrees
+  # 721 latitude steps, where each step = 0.25 degrees
+  # 493 time steps from 1979 to the present, where each step = 1 month
+  
+  nc <- nc_open(sFile)
+  
+  # print(nc) # uncomment this line if you want to see an overview of the file contents
+  
+  dname <- "t2m" # temperature at 2 meters above the earth's surface
+  dlname <- ncatt_get( nc, dname, attname="long_name", verbose=FALSE )
+  dlunits <- ncatt_get( nc, dname, attname="units", verbose=FALSE )
+  fillvalue <- ncatt_get(nc, dname, "_FillValue")
+  scale <- ncatt_get(nc, dname,"scale_factor")
+  offset <- ncatt_get(nc,dname,"add_offset")
+
+  fillvalue <- as.numeric(fillvalue)  # -32767
+  scale <- as.numeric(scale)
+  offset <- as.numeric(offset)
+  
+  # get the dimensions of the data array  
+  
+  xlon <- ncvar_get(nc, "longitude")
+  xlat <- ncvar_get(nc, "latitude")
+  xver <- ncvar_get(nc, "expver")
+  xtime <- ncvar_get(nc, "time")
+  
+  nlon <- dim(xlon)   #1440
+  nlat<- dim(xlat)    #741
+  nver<- dim(xver)    #2   version? 1, 5  
+  ntime<- dim(xtime)  #492 months
+  
+  #print(nlon)
+  #print(nlat)
+  #print(nver)
+  #print(ntime)
+  
+  # make grid of given longitude and latitude
+  
+  lonlat <- expand.grid(xlon, xlat)     
+  lonlat <- data.frame(lonlat)
+  
+  names(lonlat)[1] <- "lon"
+  names(lonlat)[2] <- "lat"
+  
+  #fetch the weather data 
+  
+  xOut <- 0
+  
+  for (yr in Year_First:Year_Last)
+  {
+    
+    for (m in Month_First:Month_Last)
+    {
+    
+      i = m + 12 * (yr - 1979)
+    
+      if (i > ntime) { break; }
+    
+      # progress upddate
+      
+      sY <- as.character(yr)
+      sM <- as.character(m)
+      sLabel <- paste("t", sY, "-", sM, sep = "")
+      
+      print(sLabel)
+    
+      # fetch the data
+      
+        #c(lon, lat, ver, time)
+      myArray <- ncvar_get(nc, dname, start=c(1, 1, 1, i), count=c(-1, -1, 1, 1))
+      
+      # convert to data frame
+      
+      myVector <- as.vector(myArray) 
+      myMatrix <- matrix(myVector, nrow = nlon * nlat, ncol = 1)
+      myDF <- data.frame(myMatrix)
+
+      # x <- colCounts(myMatrix, value = -32767)
+    
+      # adjust values using scale factor and offset
+    
+      myDF <- myDF * scale
+      myDF <- myDF + offset
+      
+      # convert from Kelvin to Celsius
+    
+      myDF <- myDF - 273.15
+      
+      # insert column name for this month of data
+      
+      names(myDF)[1] <- sLabel
+    
+      # drop some lat/lon points, leaving us with with 1 degree spacing instead of 0.25 degrees
+    
+      myDF <- cbind(lonlat, myDF)
+      
+      if (UseOneDegreeSpacing == TRUE)
+      {
+        myDF <- myDF[ abs( myDF$lon - round(myDF$lon ) ) < 0.00000001,  ]
+        myDF <- myDF[ abs( myDF$lat - round(myDF$lat ) ) < 0.00000001,  ]
+      }
+      
+      # drop lat/lon points that are outside of user-specified rectangle
+      
+      if (Extract_Rectangular_Area == TRUE)
+      {
+        myDF <- myDF[ myDF$lat >= Area_Lat_Min,  ]
+        myDF <- myDF[ myDF$lat <= Area_Lat_Max,  ]
+        myDF <- myDF[ myDF$lon >= Area_Lon_Min,  ]
+        myDF <- myDF[ myDF$lon <= Area_Lon_Max,  ]
+      }
+    
+      # add this month's data to output array
+      
+      if (yr == Year_First && m == Month_First )
+      {
+        xOut <- myDF 
+      }
+      else
+      {
+        myDF <- myDF[c(3)]
+        xOut <- cbind(xOut, myDF)
+      }
+      
+    }
+    
+  }
+  
+  nc_close(nc)
+  
+  #xOut <- round(xOut, 2)
+
+  # output
+  
+  write.csv(xOut, file = CSV_Output_File, row.names = FALSE)
+
+}  
+
+
+
+round_df <- function(df, digits) 
+{
+  
+  nums <- vapply(df, is.numeric, FUN.VALUE = logical(1))
+  
+  df[,nums] <- round(df[,nums], digits = digits)
+  
+  (df)
+  
+}
+
